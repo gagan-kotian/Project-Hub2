a9d1e3e29543064b14269b62f3f26ac5e7fac7cd
Gagan <142842464+gagan-kotian@users.noreply.github.com>
2024-12-26 20:11:13 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,17 +8,17 @@ const Home = () => {
         <h1 class="text-3xl md:text-5xl font-bold text-center mb-8 text-blue-900">
           PROJECT ZONE
         </h1>
-        <div class="flex flex-col md:flex-row items-center justify-center">
-          <a
-            class="btn btn-primary mb-4 md:mb-0 md:mr-4"
-            href="/employer/dashboard"
-          >
-            New Project
-          </a>
-          <a class="btn btn-secondary" href="/employee/feed">
-            View Projects
-          </a>
-        </div>
+        // <div class="flex flex-col md:flex-row items-center justify-center">
+        //   <a
+        //     class="btn btn-primary mb-4 md:mb-0 md:mr-4"
+        //     href="/employer/dashboard"
+        //   >
+        //     New Project
+        //   </a>
+        //   <a class="btn btn-secondary" href="/employee/feed">
+        //     View Projects
+        //   </a>
+        // </div>
       </div>
     </div>
   );
