88e7faba07b73d96fe176af423c59390299b414f
Gagan <142842464+gagan-kotian@users.noreply.github.com>
2024-12-26 20:15:08 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,22 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div id="root">
-      <div class="bg-blue-100 min-h-screen flex flex-col justify-center items-center font-serif">
-        <h1 class="text-3xl md:text-5xl font-bold text-center mb-8 text-blue-900">
-          PROJECT ZONE
-        </h1>
-        // <div class="flex flex-col md:flex-row items-center justify-center">
-        //   <a
-        //     class="btn btn-primary mb-4 md:mb-0 md:mr-4"
-        //     href="/employer/dashboard"
-        //   >
-        //     New Project
-        //   </a>
-        //   <a class="btn btn-secondary" href="/employee/feed">
-        //     View Projects
-        //   </a>
-        // </div>
-      </div>
+      Hi
     </div>
   );
 };
